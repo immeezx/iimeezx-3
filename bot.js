18abb5f0c60db131513775c756689daeff1db656
immeezx <47362425+immeezx@users.noreply.github.com>
2019-02-10 14:50:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const client = new Discord.Client();
-var prefix = "m.";
+var prefix = "^";
 var dat = JSON.parse("{}");
 function forEachObject(obj, func) {
     Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
@@ -137,13 +137,7 @@ client.on("message", message => {
    
    **
    
-╭━━━┳╮╱╱╱╱╱╱╱ ╭━━╮╱╱╱╱╭╮
-┃╭━╮┃┃╱╱╱╱╱╱╱ ┃╭╮┃╱╱╱╭╯╰╮
-┃╰━╯┃┃╭╮╭┳━━╮ ┃╰╯╰┳━━╋╮╭╯
-┃╭━━┫┃┃┃┃┃━━┫ ┃╭━╮┃╭╮┃┃┃
-┃┃╱╱┃╰┫╰╯┣━━┃ ┃╰━╯┃╰╯┃┃╰╮
-╰╯╱╱╰━┻━━┻━━╯ ╰━━━┻━━╯╰━╯
-   **
+
    
            ***General Commands***
    **
